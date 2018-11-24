9bab63e7b2d5e0f858a7f45ba0513f4a239d8249
Anysia V <anysiavalko@gmail.com>
2018-11-24 03:50:53 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,9 @@
 class Book
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
   
-  def initialize(b_title) 
-   @title = b_title
-  end
-  
-  def title= (title)
-    @title = title
-  end
-  
-  def title
-    @title
+  def initialize(title) 
+   @title = title
   end
   
   def turn_page
